2476e63960b7d1a4ba428ec0d7ce427732c0f1ae
elginloh <elginloh@gmail.com>
2021-06-22 16:23:50 +0800

    Added layout for Register

--- app/src/main/java/sg/edu/triofit/RegisterActivity.java
+++ app/src/main/java/sg/edu/triofit/RegisterActivity.java
@@ -2,7 +2,10 @@ package sg.edu.triofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
