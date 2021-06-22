41627d396e78d0c0a9abbdf12ef03f5b5a89f8f5
elginloh <elginloh@gmail.com>
2021-06-22 15:59:47 +0800

    Added layout for Login

--- app/src/main/java/sg/edu/triofit/MainActivity.java
+++ app/src/main/java/sg/edu/triofit/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.triofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
