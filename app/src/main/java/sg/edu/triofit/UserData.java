76bacdd1b6a1f47d91dc344072cad1d44f3ba297
elginloh <elginloh@gmail.com>
2021-06-25 16:43:22 +0800

    updated userdata with more information

--- app/src/main/java/sg/edu/triofit/UserData.java
+++ app/src/main/java/sg/edu/triofit/UserData.java
@@ -1,8 +1,12 @@
 package sg.edu.triofit;
 
+import java.util.Date;
+
 public class UserData {
     private String Username;
     private String Password;
+    private String Email;
+    private String DOB;
 
     public String getUsername() {
         return Username;
@@ -20,11 +24,31 @@ public class UserData {
         Password = password;
     }
 
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
     public UserData() {
     }
 
-    public UserData(String username, String password) {
+    public UserData(String username, String password, String email, String DOB) {
         Username = username;
         Password = password;
+        Email = email;
+        this.DOB = DOB;
     }
+
+
 }
