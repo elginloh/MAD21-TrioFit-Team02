122086768bb5abd311f9413417f11b2a0829adcc
elginloh <elginloh@gmail.com>
2021-06-25 16:42:35 +0800

    Functions of Finduser, Adduser and database created (done)

--- app/src/main/java/sg/edu/triofit/DBHandler.java
+++ app/src/main/java/sg/edu/triofit/DBHandler.java
@@ -6,12 +6,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
+
 public class DBHandler extends SQLiteOpenHelper {
 
     public static String DATABASE_NAME = "accountsDB.db";
     public static String ACCOUNTS = "Accounts";
     public static String COLUMN_USERNAME = "Username";
     public static String COLUMN_PASSWORD = "Password";
+    public static String COLUMN_EMAIL = "Email";
+    public static String COLUMN_DATE = "DOB";
     public static int DATABASE_VERSION = 1;
 
     public  DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
@@ -22,8 +27,8 @@ public class DBHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db){
         String CREATE_ACCOUNTS_TABLE = "CREATE TABLE " + ACCOUNTS + "(" + COLUMN_USERNAME + " TEXT,"
-                + COLUMN_PASSWORD + " TEXT" + ")";
-        //CREATE TABLE Accounts(Username TEXT, Password TEXT)
+                + COLUMN_PASSWORD + " TEXT," + COLUMN_EMAIL + " TEXT," + COLUMN_DATE + " TEXT"+ ")";
+        //CREATE TABLE Accounts(Username TEXT, Password TEXT, Email TEXT, Date TEXT)
 
         db.execSQL(CREATE_ACCOUNTS_TABLE);
     }
@@ -39,24 +44,31 @@ public class DBHandler extends SQLiteOpenHelper {
         ContentValues values = new ContentValues();
         values.put(COLUMN_USERNAME, userData.getUsername());
         values.put(COLUMN_PASSWORD, userData.getPassword());
+        values.put(COLUMN_EMAIL, userData.getEmail());
+        values.put(COLUMN_DATE, userData.getDOB());
 
         SQLiteDatabase db = this.getWritableDatabase();
         db.insert(ACCOUNTS,  null, values);
         db.close();
     }
 
-    public UserData findUser(String username)
+    public UserData findUser(String username, String email)
     {
-        String query = "SELECT * FROM " + ACCOUNTS + " WHERE " + COLUMN_USERNAME +
-                "=\"" + username + "\"";
+        String query1 = "SELECT * FROM " + ACCOUNTS + " WHERE " + COLUMN_USERNAME +
+                "=\"" + username + "\"" + " OR " + COLUMN_EMAIL + "=\"" + email + "\"";
+
+
         //SELECT * FROM Accounts WHERE username = " ??? "
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
+        Cursor cursor = db.rawQuery(query1, null);
         UserData queryData = new UserData();
-        if (cursor.moveToFirst())
+        if (cursor.moveToFirst()) // to check if user exists
         {
             queryData.setUsername(cursor.getString(0));
             queryData.setPassword(cursor.getString(1));
+            queryData.setEmail(cursor.getString(2));
+            queryData.setDOB(cursor.getString(3));
+            //extract information of user
             cursor.close();
         }
         else
@@ -66,5 +78,6 @@ public class DBHandler extends SQLiteOpenHelper {
         db.close();;
         return queryData;
     }
+
 
 }
