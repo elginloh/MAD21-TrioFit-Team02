38bdcc9b341a3fe17f243f1b0193d62c75fa6613
elginloh <elginloh@gmail.com>
2021-07-02 21:09:41 +0800

    added function to check bmi and change the bmistatus message

--- app/src/main/java/sg/edu/triofit/MainActivity.java
+++ app/src/main/java/sg/edu/triofit/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.triofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView welcomeusername, bmistatus;
+        welcomeusername = findViewById(R.id.wlcuser);
+        bmistatus = findViewById(R.id.bmistatus);
+        welcomeusername.setText("Welcome back, " + "elgin");
+    }
+
+    public String bmiCheck(Double bmi){
+        String message = "";
+        if(bmi < 18.5)
+        {
+            message = "You need to eat more stuff!";
+        }
+        else if(bmi < 24.9)
+        {
+            message = "Your BMI is looking good!";
+        }
+        else if(bmi < 29.9)
+        {
+            message = "You need to eat less";
+        }
+        else
+        {
+            message = "You need to exercise and eat healthy";
+        }
+        return message;
     }
 }
