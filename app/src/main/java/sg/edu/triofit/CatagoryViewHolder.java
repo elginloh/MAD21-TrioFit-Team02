14be780ff6f732d150176e8e8ea108fb0b242158
Shi Xian Ryan Seem <s10205740@connect.np.edu.sg>
2021-07-03 21:02:16 +0800

    Creation of catagory viewholder (only layout done)

---
+++ app/src/main/java/sg/edu/triofit/CatagoryViewHolder.java
@@ -0,0 +1,14 @@
+package sg.edu.triofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatagoryViewHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catagory_view_holder);
+    }
+}
