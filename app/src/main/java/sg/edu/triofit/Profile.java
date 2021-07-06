97c13e466b7eb87e3f27f94a657eeb8698cf174a
elginloh <elginloh@gmail.com>
2021-07-06 23:38:17 +0800

    set default text for profile and added camera icon

--- app/src/main/java/sg/edu/triofit/Profile.java
+++ app/src/main/java/sg/edu/triofit/Profile.java
@@ -3,6 +3,9 @@ package sg.edu.triofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        /*---------------Variables---------------*/
+        TextView accountName, accountNumber, accountEmail, accountAge, accountBMI, accountHeight, accountWeight, profileName, profileEmail;
+
+        /*---------------Variables---------------*/
+
+        //Retrieve from layout
+        accountName = findViewById(R.id.accountName);
+        accountNumber = findViewById(R.id.accountNumber);
+        accountEmail = findViewById(R.id.accountEmail);
+        accountAge = findViewById(R.id.accountAge);
+        accountBMI = findViewById(R.id.accountBMI);
+        accountHeight = findViewById(R.id.accountHeight);
+        accountWeight = findViewById(R.id.accountWeight);
+        profileName = findViewById(R.id.profileName);
+        profileEmail = findViewById(R.id.profileEmail);
+
+        //set account information
+        profileName.setText("Elgin Loh");
+        profileEmail.setText("elginloh@gmail.com");
+        accountName.setText("Elgin Loh");
+        accountNumber.setText("91691626");
+        accountEmail.setText("elginloh@gmail.com");
+        accountAge.setText("20");
+        accountBMI.setText("24");
+        accountHeight.setText("173CM");
+        accountWeight.setText("73KG");
+
     }
 }
