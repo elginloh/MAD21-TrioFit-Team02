bd72fcfd488628c11f96d536ed42a5e2477999a5
Ng Jun Heng <s10208354@connect.np.edu.sg>
2021-07-09 10:46:19 +0800

    Update UserData class

--- app/src/main/java/sg/edu/triofit/UserData.java
+++ app/src/main/java/sg/edu/triofit/UserData.java
@@ -6,7 +6,9 @@ public class UserData {
     private String Username;
     private String Password;
     private String Email;
-    private String DOB;
+    private String Age;
+    private float height;
+    private float weight;
 
     public String getUsername() {
         return Username;
@@ -32,23 +34,39 @@ public class UserData {
         Email = email;
     }
 
-    public String getDOB() {
-        return DOB;
+    public String getAge() {
+        return Age;
     }
 
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
     }
 
     public UserData() {
     }
 
-    public UserData(String username, String password, String email, String DOB) {
+    public UserData(String username, String password, String email, String age, float height, float weight) {
         Username = username;
         Password = password;
         Email = email;
-        this.DOB = DOB;
+        Age = age;
+        this.height = height;
+        this.weight = weight;
     }
-
-
 }
